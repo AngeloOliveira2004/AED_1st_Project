1dda5e041228eba3674e7b8eac53241d33776116
JoseCostaUni <146367084+JoseCostaUni@users.noreply.github.com>
2023-10-26 12:23:33 +0100

    Add files via upload

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/angelo/Downloads/Universidade/Algoritmo e Estrutura de Dados/Projeto_AED
+# Install script for directory: /home/jose-costa/Documents/Uni/AED/Projeto
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/angelo/Downloads/Universidade/Algoritmo e Estrutura de Dados/Projeto_AED/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jose-costa/Documents/Uni/AED/Projeto/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
